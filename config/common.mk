bf5c3f3c8ab03457abfceff76cae1a125b2ed549
iamharshdev <nitingoswami473@gmail.com>
2020-08-24 16:00:05 +0000

    added pe bootaniamtion
    Change-Id: Iaf2e9ac077219e2cf4f21ceccb07494f199a1eb9

--- config/common.mk
+++ config/common.mk
@@ -102,8 +102,21 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Bootanimation
-PRODUCT_PACKAGES += \
-    bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+    PRODUCT_COPY_FILES += vendor/lineage/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+    PRODUCT_COPY_FILES += vendor/lineage/prebuilt/common/bootanimation/1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+    PRODUCT_COPY_FILES += vendor/lineage/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += \
+        vendor/lineage/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
 
 # AOSP packages
 PRODUCT_PACKAGES += \
