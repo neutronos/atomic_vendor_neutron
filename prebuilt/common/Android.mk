235c7dfa013bed72a08ec9c2e06f5f79c850a449
iamharshdev <nitingoswami473@gmail.com>
2020-08-31 03:53:56 +0000

    added longshot updated walldrops

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -67,17 +67,32 @@ LOCAL_CERTIFICATE   := platform
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MULTILIB := both
+LOCAL_REQUIRED_MODULES := app/com.electronlabs.walldrops.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := Longshot
-LOCAL_SRC_FILES     := app/Longshot.apk
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := APPS
-LOCAL_CERTIFICATE   := platform
-LOCAL_DEX_PREOPT := false
+LOCAL_MODULE := com.electronlabs.walldrops.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := app/com.electronlabs.walldrops.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Longshot
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := app/Longshot.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MULTILIB := both
+LOCAL_DEX_PREOPT := false
+LOCAL_REQUIRED_MODULES := app/com.android.screenshot.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := org.android.screenshot.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := app/com.android.screenshot.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
