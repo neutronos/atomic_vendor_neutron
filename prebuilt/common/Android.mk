83d43d8ccfea6b16063039ca3e09c89f795be48b
iamharshdev <nitingoswami473@gmail.com>
2020-08-31 06:17:49 +0000

    removed longshot

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -78,24 +78,6 @@ LOCAL_SRC_FILES := app/com.electronlabs.walldrops.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := Longshot
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := app/Longshot.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_DEX_PREOPT := false
-LOCAL_REQUIRED_MODULES := app/com.android.screenshot.xml
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := org.android.screenshot.xml
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-LOCAL_SRC_FILES := app/com.android.screenshot.xml
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE        := ScreenRecorder
 LOCAL_SRC_FILES     := app/ScreenRecorder.apk
 LOCAL_MODULE_TAGS   := optional
