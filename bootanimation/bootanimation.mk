d8a34272a1b47f756d4b347f58fe6d91c3c46931
iamharshdev <nitingoswami473@gmail.com>
2020-10-06 15:37:24 +0000

    Switch to pixel boot animation

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -14,11 +14,11 @@
 
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/720p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/1080p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/1440p.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     $(error "Bootanimation: TARGET_BOOT_ANIMATION_RES is undefined")
 endif
